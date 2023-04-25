db2b064c8678b9423edacc271be904afcb0aef44
“personal” <rmfhsep@gmail.com>
2023-04-25 22:38:57 +0900

    다이나믹 라우팅, 캐싱 추가

--- app/list/page.js
+++ app/list/page.js
@@ -3,9 +3,15 @@ import { connectDB } from "@/util/database";
 import Link from "next/link";
 import DetailLink from "./DetailLink";
 import ListItem from "./ListItem";
+
+export const dynamic = "force-dynamic";
+
+
 export default async function list () {
 const db = (await connectDB).db("forum")
 const result  = await db.collection('post').find().toArray()
+
+
 
      return (
        <div className="list-bg">
