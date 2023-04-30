0516f86c449f16286959e13005b1add9cfa8ba17
“personal” <rmfhsep@gmail.com>
2023-04-30 22:33:25 +0900

    에러 페이지 추가

---
+++ app/detail/[id]/error.js
@@ -0,0 +1,9 @@
+'use client'
+
+export default function Error () {
+    return (
+      <>
+        <h4>error</h4>
+      </>
+    );
+}
--- app/detail/[id]/page.js
+++ app/detail/[id]/page.js
@@ -1,11 +1,16 @@
 import { connectDB } from "@/util/database"
 import { ObjectId } from "mongodb"
 import Comment from "./Comment"
+import { notFound } from "next/navigation"
 export default async function Detail(props) {
     const db = (await connectDB).db("forum")
     const id = new ObjectId(props.params.id)
 
     const result = await db.collection("post").findOne({ _id: id });
+
+    if(result === null) {
+      return notFound()
+    }
 
     return (
       <>
